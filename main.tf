7caa93396f014ac315d01ed4602dcde0b89c8579
mauro666x <mfbenavidesc@gmail.com>
2024-12-12 19:43:37 -0500

    changes

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
+
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "cloudcamp-terraform-123123"
-
-  tags = {
-    Name        = "cloudcamp-terraform"
-    Environment = "Dev"
-  }
 }
