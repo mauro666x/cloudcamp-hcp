627076db4eb3ca9cf430a70569e19838865533e4
mauro666x <mfbenavidesc@gmail.com>
2024-12-12 20:10:59 -0500

    adding tf references

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
-terraform {
-  cloud {
-    organization = "cloudcamp-labs"
-    workspaces {
-      name = "cloudcamp-hcp"
-    }
-  }
-}
+# for connection to github
+# terraform {
+#   cloud {
+#     organization = "cloudcamp-labs"
+#     workspaces {
+#       name = "github-connection"
+#     }
+#   }
+# }
+
+#via cli, pero primero hacer un terraform login
+# terraform {
+#   cloud {
+#     organization = "cloudcamp-labs"
+#     workspaces {
+#       name = "cli"
+#     }
+#   }
+# }
+
 provider "aws" {
   region = "us-east-1"
 }
